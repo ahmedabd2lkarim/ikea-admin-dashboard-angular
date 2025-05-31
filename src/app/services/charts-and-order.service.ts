3bf29c6f0a13cf5dfaff6a603a2aba1675d20410
ahmedabd2lkarim <ahmedabd2lkarim@gmail.com>
2025-05-31 18:50:11 +0300

    update environment

--- src/app/services/charts-and-order.service.ts
+++ src/app/services/charts-and-order.service.ts
@@ -4,42 +4,46 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
-  private apiUrl = `${environment.production}/api/orders`;
-  private apiUrl2 = `${environment.production}/api/dashboard/get-details`;
+  private apiUrl = `${environment.baseURL}/api/orders`;
+  private apiUrl2 = `${environment.baseURL}/api/dashboard/get-details`;
 
-  private dashboard = `${environment.production}/api/dashboard`;
-  private adminUrl = `${environment.production}/api/admin`;
+  private dashboard = `${environment.baseURL}/api/dashboard`;
+  private adminUrl = `${environment.baseURL}/api/admin`;
 
   constructor(private http: HttpClient) {}
 
   private getHeaders() {
-    const token = localStorage.getItem('token'); 
+    const token = localStorage.getItem('token');
     console.log(token);
-    
+
     return {
       headers: new HttpHeaders({
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      })
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }),
     };
   }
 
   getAllOrders(): Observable<any> {
-
     // console.log(this.http.get(`${this.apiUrl2}`, this.getHeaders()));
-    
-    
+
     return this.http.get(`${this.apiUrl2}`, this.getHeaders());
   }
 
-  updateOrderStatus(orderId: string, status: { status: string }): Observable<any> {
-    return this.http.patch(`${this.apiUrl}/status/${orderId}`, status, this.getHeaders());
+  updateOrderStatus(
+    orderId: string,
+    status: { status: string }
+  ): Observable<any> {
+    return this.http.patch(
+      `${this.apiUrl}/status/${orderId}`,
+      status,
+      this.getHeaders()
+    );
   }
   getTotalRevenue(): Observable<any> {
-
     return this.http.get(`${this.dashboard}/total-revenue`, this.getHeaders());
   }
   getUserCount(): Observable<any> {
@@ -49,14 +53,24 @@ export class OrderService {
     return this.http.get(`${this.dashboard}/get-revenue`, this.getHeaders());
   }
   getCategoryProductCounts(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.dashboard}/product-counts`, this.getHeaders());
+    return this.http.get<any[]>(
+      `${this.dashboard}/product-counts`,
+      this.getHeaders()
+    );
   }
 
   getAdminDetails(): Observable<any> {
-    return this.http.get(`http://localhost:5000/api/auth/profile` , this.getHeaders());
+    return this.http.get(
+      `${environment.baseURL}/api/auth/profile`,
+      this.getHeaders()
+    );
   }
 
   updateAdminProfile(adminData: any): Observable<any> {
-    return this.http.patch(`http://localhost:5000/api/dashboard/update-Admin`, adminData , this.getHeaders());
+    return this.http.patch(
+      `${environment.baseURL}/api/dashboard/update-Admin`,
+      adminData,
+      this.getHeaders()
+    );
   }
 }
